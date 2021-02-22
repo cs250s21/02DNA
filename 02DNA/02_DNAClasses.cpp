264e20377b59423fff9d8def4b505fd777cab941
Vonslonaker <Vonslonaker@DESKTOP-DJ8CHMR>
2021-02-21 21:31:43 -0800

    Add project files.

---
+++ 02DNA/02_DNAClasses.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// Write the code for main here
+	// You will need to create files for .h and .cpp dna files
+	
+
+	cout << "SUCCESS!" << endl;
+	return EXIT_SUCCESS;
+}
